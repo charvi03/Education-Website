faf994d0a21a46eecdfbb3e46088de1457755dd8
charvi03 <charvikhurana01@gmail.com>
2023-04-30 12:18:49 +0530

    init

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0);
 });
+
+//show/hide faq answer
+
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+  });
+});
